d82858e543df8c46586a151512ab14e2d75b5a18
psubbo <psubbo@yandex.ru>
2018-03-31 02:53:44 +0300

    Add comments

--- js/auth.js
+++ js/auth.js
@@ -3,18 +3,24 @@ var loginInput = document.querySelector('.loginInput');
 var passwordInput = document.querySelector('.passwordInput');
 var formMessage = document.querySelector('.formMessage');
 
+
+//Ждем сабмита формы и останавливаем стандартное событие
+
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     var login = document.auth.login.value;
     var password = document.auth.password.value;
 
-    if (login.length === 0) { // проверка, заполнено ли имя
+
+    //Проверяем заполнены ли поля формы при сабмите, если нет выводим текст ошибки и красим поля ввода в стандартные ошибки Бутстрапа
+
+    if (login.length === 0) {
         formMessage.innerHTML = 'Логин не указан';
         loginInput.classList.add('has-error');
 
         formMessage.classList.remove('form-message-success');
         formMessage.classList.add('form-message-error');
-    } else if (password.length === 0) { // проверка, заполнена ли эл. почта
+    } else if (password.length === 0) {
         formMessage.innerHTML = 'Пароль не указан';
         loginInput.classList.remove('has-error');
         passwordInput.classList.add('has-error');
@@ -23,16 +29,19 @@ form.addEventListener('submit', function(ev) {
     } else {
         loginInput.classList.remove('has-error');
         passwordInput.classList.remove('has-error');
-        var authData = login + ":" + password;
-        bstring = btoa(authData);
 
+        // Если все поля заполнены то кодируем их содержание в base64 и сохраняем на время сессии в sessionStorage
+
+        var bstring = btoa(login + ":" + password);
         sessionStorage.bstring = bstring;
+
+        // Готовим параметры запроса в апи Hermes - для авторизации используем пустой запрос на метод GetStatusesByParcelBarcodes если вернет код 200 - значит корректно введены данные логина и пароля, если что то другое - значит не срослось. 
 
         var params = {
             method: 'POST',
             timeout: 60000,
             body: {
-                "parcelBarCodes": [],
+                "parcelBarCodes": [], // пустой запрос 
             },
             headers: {
                 "Authorization": "Basic " + bstring,
@@ -40,10 +49,15 @@ form.addEventListener('submit', function(ev) {
             },
         };
 
+        // Делаем запрос в API Hermes
+
         fetch('https://api.hermes-dpd.ru/ws/restservice.svc/rest/GetStatusesByParcelBarcodes', params)
             .then(
                 function(response) {
                     if (response.status !== 200) {
+
+                        // Если статус не 200 - паникуем, красим все в красный и говорим что все пропало в консоль и в сообщении в форме
+
                         console.log('Huston we have a problem. Status Code: ' +
                             response.status);
 
@@ -54,6 +68,9 @@ form.addEventListener('submit', function(ev) {
                         passwordInput.classList.toggle('has-error');
                         return;
                     } else {
+
+                        // Если все хорошо - пускаем в админку
+
                         window.location.assign("https://psubbo.github.io/dashboard.html");
                     }
 
