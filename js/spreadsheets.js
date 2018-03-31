7a47cf158aeb205dc62357475f1be822095d60d8
psubbo <psubbo@yandex.ru>
2018-03-31 17:04:23 +0300

    Загрузка по нажатию на кнопку

--- js/spreadsheets.js
+++ js/spreadsheets.js
@@ -1,28 +1,30 @@
 var API_KEY = "AIzaSyAnncvWgiQITGBVFd9W-zY3_GvVZDJ16fc";
 var spreadsheetID = "1YCzTTvcecJdOvhLYZ-J6iCzpLBmffIHXSgaijVkPfq8";
-var rowsNumber
+var button = document.querySelector('.btn')
 var range = "A2:E";
 var tbody = document.querySelector(".tbody");
-
 var apiUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + spreadsheetID + "/values/" + range + "?key=" + API_KEY;
-fetch(apiUrl)
-    .then(
-        function(response) {
-            if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
-                return;
-            }
-            // Examine the text in the response  
-            response.json().then(function(data) {
-                //console.log(data.values); // выводим содержимое таблицы в консоль
-                for (var i = 0; i < data.values.length; i++) {
-                    var row = data.values[i];
-                    // row = ['Страна', 'Столица']
 
-                    tbody.innerHTML += `
+button.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    fetch(apiUrl)
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+                // Examine the text in the response  
+                response.json().then(function(data) {
+                    //console.log(data.values); // выводим содержимое таблицы в консоль
+                    for (var i = 0; i < data.values.length; i++) {
+                        var row = data.values[i];
+                        // row = ['Страна', 'Столица']
+
+                        tbody.innerHTML += `
                      <tr>
-                       <td>${i+1}</td>
+                       <td>${i + 1}</td>
                        <td>${row[0]}</td>
                        <td>${row[1]}</td>
                        <td>${row[2]}</td>
@@ -30,11 +32,13 @@ fetch(apiUrl)
                        <td>${row[4]}</td>
                      </tr>
                    `;
-                }
+                    }
 
-            });
-        }
-    )
-    .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-    });
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+
+});
