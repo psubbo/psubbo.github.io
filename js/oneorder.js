6df4a1ec86266db64af3c3625019e3aedbcbab74
psubbo <psubbo@yandex.ru>
2018-03-31 21:46:52 +0300

    oneorder start

--- js/oneorder.js
+++ js/oneorder.js
@@ -5,67 +5,63 @@ var range = "A2:E";
 var tbody = document.querySelector(".tbody");
 var apiUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + spreadsheetID + "/values/" + range + "?key=" + API_KEY;
 var bstring = sessionStorage.bstring;
+var form = document.getElementsByTagName('form');
 
-fetch(apiUrl)
-    .then(
-        function(response) {
-            if (response.status !== 200) {
-                console.log('Unable to fetch Google Spreadsheets. Status Code: ' +
-                    response.status);
-                return;
-            }
-
-            response.json().then(function(a) {
-
-                for (var i = 0; i < a.values.length; i++) {
-                    var row = a.values[i];
-                    var orderNumber = row[0];
-                    var trackCode = row[1];
-                    var shippingDate = row[2];
-                    var custName = row[3];
-                    var deliveryCity = row[4];
-                    debugger;
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    var order = document.searchForm.search.value;
 
 
 
+    debugger;
 
-
-                    var data = "{\r\n\t\"parcelBarCodes\":[" + trackCode + "],\r\n}";
-                    var statusString;
-                    var xhr = new XMLHttpRequest();
-
-                    xhr.addEventListener("readystatechange", function() {
-                        if (this.readyState === 4) {
-                            var status = JSON.parse(this.responseText);
-                            statusString = status.GetStatusesByParcelBarcodesResult[0].StatusName
-                            debugger;
-                        }
-                    });
-                    var authHeader = "Basic " + bstring;
-                    xhr.open("POST", "https://api.hermes-dpd.ru/ws/restservice.svc/rest/GetStatusesByParcelBarcodes", false);
-                    xhr.setRequestHeader("Content-Type", "application/json");
-                    xhr.setRequestHeader("Authorization", authHeader);
-
-
-
-                    xhr.send(data);
-
-                    tbody.innerHTML += `
-                     <tr>
-                       <td>${i + 1}</td>
-                       <td>${row[0]}</td>
-                       <td>${row[1]}</td>
-                       <td>${row[2]}</td>
-                       <td>${row[3]}</td>
-                       <td>${row[4]}</td>
-                       <td>${statusString}</td>
-                     </tr>
-                   `;
+    fetch(apiUrl)
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                    console.log('Unable to fetch Google Spreadsheets. Status Code: ' +
+                        response.status);
+                    return;
                 }
 
-            });
-        }
-    )
-    .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-    });
+                response.json().then(function(a) {
+
+                    for (var i = 0; i < a.values.length; i++) {
+                        var row = a.values[i];
+                        var orderNumber = row[0];
+                        var trackCode = row[1];
+                        var shippingDate = row[2];
+                        var custName = row[3];
+                        var deliveryCity = row[4];
+                        var statusString;
+                        var data = "{\r\n\t\"parcelBarCodes\":[" + trackCode + "],\r\n}";
+
+
+
+
+
+                        var xhr = new XMLHttpRequest();
+
+                        xhr.addEventListener("readystatechange", function() {
+                            if (this.readyState === 4) {
+                                var status = JSON.parse(this.responseText);
+                                statusString = status.GetStatusesByParcelBarcodesResult[0].StatusName
+                            }
+                        });
+                        var authHeader = "Basic " + bstring;
+                        xhr.open("POST", "https://api.hermes-dpd.ru/ws/restservice.svc/rest/GetStatusesByParcelBarcodes", false);
+                        xhr.setRequestHeader("Content-Type", "application/json");
+                        xhr.setRequestHeader("Authorization", authHeader);
+                        xhr.send(data);
+
+                    }
+
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+
+
+});
