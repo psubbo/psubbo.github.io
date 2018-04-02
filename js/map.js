13c5400960590703df3b6bf979eef94c128dfa98
psubbo <psubbo@yandex.ru>
2018-04-02 23:07:18 +0300

    Map with Hermes points done

--- js/map.js
+++ js/map.js
@@ -1,52 +1,68 @@
-var bstring = localStorage.bstring;
-var parcelShops;
-
-function updateParcelShops(bstring) {
-
-    var data = "{\r\n\t\"businessUnitCode\":\"2840\",\r\n}";
-
-    var xhr = new XMLHttpRequest();
-
-    var authHeader = "Basic " + bstring;
-    xhr.open("POST", "https://api.hermes-dpd.ru/ws/restservice.svc/rest/GetParcelShops");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Authorization", authHeader);
-    xhr.addEventListener("readystatechange", function() {
-
-        if (this.readyState === 4) {
-
-            parcelShops = JSON.parse(this.responseText).GetParcelShopsResult;
-            debugger;
-
-        }
-        return parcelShops;
-    });
-
-    xhr.send(data);
+    var bstring = localStorage.bstring;
+    var parcelShops;
 
 
-};
+    //Почему fetch не работает а XMLHttpRequest() работает? ЧЯДНТ?
 
-updateParcelShops(bstring);
+    /* var params = {
+        method: 'POST',
+        timeout: 60000,
+        body: { "businessUnitCode": 2840 },
+        headers: {
+            "Authorization": "Basic " + bstring,
+            "Content-Type": "application/json"
+        },
+    };
+
+    fetch("https://api.hermes-dpd.ru/ws/restservice.svc/rest/GetParcelShops", params)
+        .then(
+            function(response) {
+                parcelShops = JSON.parse(response.responseText).GetParcelShopsResult;
+
+            }); */
 
 
+    function updateParcelShops(bstring) {
 
+        var data = "{\r\n\t\"businessUnitCode\":\"2840\",\r\n}";
 
+        var xhr = new XMLHttpRequest();
 
+        var authHeader = "Basic " + bstring;
+        xhr.open("POST", "https://api.hermes-dpd.ru/ws/restservice.svc/rest/GetParcelShops", false);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Authorization", authHeader);
+        xhr.addEventListener("readystatechange", function() {
 
-ymaps.ready(init);
-var myMap, myPlacemark;
+            if (this.readyState === 4) {
 
-function init() {
-    myMap = new ymaps.Map("map", {
-        center: [55.913021, 37.577787],
-        zoom: 4
-    });
+                parcelShops = JSON.parse(this.responseText).GetParcelShopsResult;
+            };
+        });
 
-    myPlacemark = new ymaps.Placemark([55.913021, 37.577787], {
-        hintContent: 'Декатлон Алтуфьево',
-        balloonContent: 'Декатлон Алтуфьево'
-    });
+        xhr.send(data);
+    };
+    updateParcelShops(bstring);
 
-    myMap.geoObjects.add(myPlacemark);
-}
+    ymaps.ready(init);
+    var myMap;
+
+    function init() {
+        myMap = new ymaps.Map("map", {
+            center: [55.4507, 37.3656],
+            zoom: 4,
+            controls: ['zoomControl', 'searchControl']
+        });
+
+        for (var i = 0; i < parcelShops.length; i++) {
+
+            var a, b;
+            var myPlacemark = new ymaps.Placemark([parcelShops[i].Latitude, parcelShops[i].Longitude], {
+                hintContent: parcelShops[i].ParcelShopName,
+                balloonContent: parcelShops[i].ExtraParams[0].Value,
+                services: parcelShops[i].Services
+
+            });
+            myMap.geoObjects.add(myPlacemark);
+        };
+    }
